33a4436a549380b722ec965c41f87d3bdfcc9dd7
PEZ <pez@pezius.com>
2012-02-21 22:26:23 +0100

    Only sending taglines to users who are online.

--- src/tasks/tagliner.py
+++ src/tasks/tagliner.py
@@ -8,7 +8,6 @@ Created on Feb 19, 2012
 
 from google.appengine.api import xmpp
 from moo.chat import Chat
-from moo.user import User
 import moo.messages as messages
 import wsgiref.handlers
 from google.appengine.ext import webapp
@@ -27,7 +26,8 @@ class Tagliner(webapp.RequestHandler):
         chats = get_chats_with_taglines()
         for chat in chats:
             tagline = get_random_tagline(chat)
-            xmpp.send_message(chat.participants, messages.TAGLINE_MESSAGE % tagline, chat.jid)
+            listeners = [u for u in chat.listeners if xmpp.get_presence(u.address, chat.jid)]
+            xmpp.send_message(listeners, messages.TAGLINE_MESSAGE % tagline, chat.jid)
 
 def main():
     app = webapp.WSGIApplication([
